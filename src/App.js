39f7e4f43b176b9bf2c430695056e752c78aa10e
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-04-18 01:05:31 +0200

    login logout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import HomeScreen from './pages/HomeScreen'
 import ProductScreen from './pages/ProductScreen'
 import CartScreen from './pages/CartScreen'
+import LoginScreen from './pages/LoginScreen'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/products/:id/' component={ProductScreen} />
           <Route path='/cart/:id?/' component={CartScreen} />
         </Container>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 /** @format */
-import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-
+import { logout } from "../actions/userActions";
 function Header() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -20,11 +29,22 @@ function Header() {
                   <i className="fab fa-opencart"></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i class="fas fa-user-alt"></i> LogIn
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i class="fas fa-user-alt"></i> LogIn
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
