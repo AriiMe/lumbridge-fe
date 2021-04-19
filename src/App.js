267912a477f088b92c54466dde433ab568ffd3c3
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-04-19 17:52:47 +0200

    login register and update profile

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import HomeScreen from './pages/HomeScreen'
 import ProductScreen from './pages/ProductScreen'
 import CartScreen from './pages/CartScreen'
 import LoginScreen from './pages/LoginScreen'
+import RegisterScreen from './pages/RegisterScreen'
+import ProfileScreen from './pages/ProfileScreen'
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/products/:id/' component={ProductScreen} />
           <Route path='/cart/:id?/' component={CartScreen} />
         </Container>
