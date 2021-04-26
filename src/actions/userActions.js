bcdcf71c6ad6c714b40f26719cfb58efe5fa37f4
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-04-26 14:53:19 +0200

    orders

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -23,6 +23,8 @@ import {
     USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
+
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({
@@ -67,6 +69,8 @@ export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
+
 }
 
 export const register = (name, email, password) => async (dispatch) => {
