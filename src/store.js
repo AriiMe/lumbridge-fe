b5bdda1ff51182b38e8a36e383013183044df367
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-04-27 14:24:26 +0200

    delete users

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ import {
     productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers'
 
 
@@ -23,6 +23,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
+    userDelete: userDeleteReducer,
 
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
