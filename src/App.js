d60308e96fa7aa79b3f9b901c51f0c07820d0aca
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-04-27 16:01:43 +0200

    product list for admins

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './pages/PlaceOrderScreen'
 import OrderScreen from './pages/OrderScreen'
 import UserListScreen from './pages/UserListScreen'
 import UserEditScreen from './pages/UserEditScreen'
+import ProductListScreen from './pages/ProductListScreen'
 
 function App() {
   return (
@@ -34,6 +35,8 @@ function App() {
 
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+
+          <Route path='/admin/productlist' component={ProductListScreen} />
         </Container>
       </main>
       <Footer />
